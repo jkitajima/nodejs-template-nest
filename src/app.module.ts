91d8ef96a4a94ab2cad984c19d080be9f44427a5
Kitajima 北島 <msg@kitajima.dev>
2024-03-30 15:59:15 -0300

    Added Prisma integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { validate } from './config/server/env.validation';
+import { PrismaModule } from './config/prisma/prisma.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: ['.env.dev', '.env.prod'],
+      envFilePath: ['.env', '.env.dev', '.env.prod'],
       cache: true,
       validate,
     }),
+    PrismaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/config/server/env.validation.ts
+++ src/config/server/env.validation.ts
@@ -1,5 +1,12 @@
 import { plainToInstance } from 'class-transformer';
-import { IsEnum, IsNumber, Max, Min, validateSync } from 'class-validator';
+import {
+  IsEnum,
+  IsNumber,
+  IsString,
+  Max,
+  Min,
+  validateSync,
+} from 'class-validator';
 
 enum Environment {
   Development = 'dev',
@@ -16,9 +23,13 @@ export class EnvironmentVariables {
   @Max(65535)
   PORT: number;
 
-  constructor(env: Environment, port: number) {
+  @IsString()
+  DATABASE_URL: string;
+
+  constructor(env: Environment, port: number, dbUrl: string) {
     this.NODE_ENV = env;
     this.PORT = port;
+    this.DATABASE_URL = dbUrl;
   }
 }
 
